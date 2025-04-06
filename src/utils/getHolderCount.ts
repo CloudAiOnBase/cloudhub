03a0393a63c6fc2cba7368f8c45017f4f48ed03d
Mars <alfamastudio11@gmail.com>
2025-04-06 23:53:53 +0200

    tweaking axio headers

--- src/utils/getHolderCount.ts
+++ src/utils/getHolderCount.ts
@@ -9,7 +9,12 @@ export async function getHolderCount(): Promise<number | null> {
     const res = await axios.get(url, {
       headers: {
         'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0 Safari/537.36',
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
       },
     });
 
